c423c2c7617776f60dee5b9815ab9e933c826c66
Amol Mane <amolmane@alumni.ubc.ca>
2016-11-22 15:55:18 -0800

    README left

--- R/get_standard_deviation.R
+++ R/get_standard_deviation.R
@@ -1,3 +1,12 @@
+#' Return standard deviation of an input vector/list.
+#'
+#' @param x vector/list of numeric
+#'
+#' @return numeric
+#' @export
+#'
+#' @examples
+#' standard_deviation(c(1,2,3,4))
 standard_deviation <- function(x) {
   n <- length(x)
   mean = sum(x) / n
