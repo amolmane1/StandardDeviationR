fae7942207781e1ba85c51629eed81b49d05e5f9
Amol Mane <amolmane@alumni.ubc.ca>
2016-12-19 00:21:57 +0400

    added tests for exercise 0

--- R/StandardDeviationR-package.r
+++ R/StandardDeviationR-package.r
@@ -1,6 +1,7 @@
 #' StandardDeviationR.
 #'
-#' Calculates the standard deviation of a vector/list input. I made this to practice making packages in R.
+#' Calculates the standard deviation of a vector/list input. I made this to 
+#' practice making packages in R.
 #'
 #' @name StandardDeviationR
 #' @docType package
--- tests/testthat/test_standard_deviation.R
+++ tests/testthat/test_standard_deviation.R
@@ -1,8 +1,117 @@
 context("Finding standard deviation")
 
-test_that("standard_deviation correctly calculates standard deviation", {
-  q <- c(1,1,3,3)
-  ans <- 1
-
-  expect_identical(standard_deviation(q), ans)
+# Valid test cases
+test_that("Simplest test case on edge condition", {
+  expect_identical(standard_deviation(c(0)), 0)
 })
+
+test_that("Simplest test case on standard condition", {
+  expect_identical(standard_deviation(c(1)), 0)
+})
+
+test_that("Multi element identical test case on edge condition", {
+  expect_identical(standard_deviation(c(0,0,0)), 0)
+})
+
+test_that("Multi element identical test case on standard condition", {
+  expect_identical(standard_deviation(c(1,1,1)), 0)
+})
+
+test_that("Multi element identical test case on standard condition", {
+  expect_identical(standard_deviation(c(0,1,2)), 0.816496580927726)
+})
+
+test_that("Multi element with negative number test case", {
+  expect_identical(standard_deviation(c(0,-1,-2)), 0.816496580927726)
+})
+
+test_that("Large size array test case", {
+  expect_identical(standard_deviation(c(0:10000)), 2887)
+})
+
+
+
+# Error Test Cases
+# test_that("No Element should return zero division error", {
+#   expect_error(standard_deviation(c()), "ZeroDivisionError")
+# })
+# 
+# test_that("Empty string should return ZeroDivisionError", {
+#   expect_error(standard_deviation(""), "ZeroDivisionError")
+# })
+# 
+# test_that("Empty set should return ZeroDivisionError", {
+#   expect_error(standard_deviation({}), "ZeroDivisionError")
+# })
+# 
+# test_that("Different type than Number inside input should return type error", {
+#   expect_error(standard_deviation(c("")), "TypeError")
+# })
+# 
+# test_that("Different type than Number inside input should return type error", {
+#   expect_error(standard_deviation(c(0, "")), "TypeError")
+# })
+# 
+# test_that("No input should return type error", {
+#   expect_error(standard_deviation(), "TypeError")
+# })
+
+
+
+context("Finding standard error")
+
+# Valid test cases
+test_that("Simplest test case on edge condition", {
+  expect_identical(standard_error(c(0)), 0)
+})
+
+test_that("Simplest test case on standard condition", {
+  expect_identical(standard_error(c(1)), 0)
+})
+
+test_that("Multi element identical test case on edge condition", {
+  expect_identical(standard_error(c(0,0,0)), 0)
+})
+
+test_that("Multi element identical test case on standard condition", {
+  expect_identical(standard_error(c(1,1,1)), 0)
+})
+
+test_that("Multi element identical test case on standard condition", {
+  expect_identical(standard_error(c(0,1,2)), 0.47140452079103173)
+})
+
+test_that("Multi element with negative number test case", {
+  expect_identical(standard_error(c(0,-1,-2)), 0.47140452079103173)
+})
+
+test_that("Large size array test case", {
+  expect_identical(standard_error(c(0:10000)), 28.86751)
+})
+
+
+
+# Error Test Cases
+# test_that("No Element should return zero division error", {
+#   expect_error(standard_error(c()), "ZeroDivisionError")
+# })
+# 
+# test_that("Empty string should return ZeroDivisionError", {
+#   expect_error(standard_error(""), "ZeroDivisionError")
+# })
+# 
+# test_that("Empty set should return ZeroDivisionError", {
+#   expect_error(standard_error({}), "ZeroDivisionError")
+# })
+# 
+# test_that("Different type than Number inside input should return type error", {
+#   expect_error(standard_error(c("")), "TypeError")
+# })
+# 
+# test_that("Different type than Number inside input should return type error", {
+#   expect_error(standard_error(c(0, "")), "TypeError")
+# })
+# 
+# test_that("No input should return type error", {
+#   expect_error(standard_error(), "TypeError")
+# })
